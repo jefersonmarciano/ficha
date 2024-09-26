4730448827323e814a2b44ac60072744131e5965
Jeferson marciano <44215511+jefersonmarciano@users.noreply.github.com>
2024-09-26 01:33:19 -0300

    correção magia ciclu 2 e responsividade

--- src/components/tabelas de magias/MagicTablePopupCiclo2Magico.js
+++ src/components/tabelas de magias/MagicTablePopupCiclo2Magico.js
@@ -1,27 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './MagicTablePopup.css'; // Reutiliza os estilos do popup
 import MagicTable from '../model table/MagicTable';
 
-const MagicTablePopupCiclo2Magico = ({ magias, onClose }) => {
-  const [selectedSkills, setSelectedSkills] = useState([]);
-
-  const handleSelect = (magia) => {
-    setSelectedSkills((prevSelectedSkills) => {
-      if (prevSelectedSkills.some(skill => skill.magia === magia.magia)) {
-        return prevSelectedSkills.filter(skill => skill.magia !== magia.magia);
-      } else {
-        return [...prevSelectedSkills, magia];
-      }
-    });
-  };
-
+const MagicTablePopupCiclo2Magico = ({ magias, onClose, onSelect, selectedSkills }) => {
   return (
     <div className="magic-table-popup-overlay">
       <div className="magic-table-popup">
         <button className="close-button" onClick={onClose}>X</button>
         <h2>Magias de Ciclo 2 - Mágico</h2>
         <div className="magic-table-container">
-          <MagicTable magias={magias} onSelect={handleSelect} selectedSkills={selectedSkills} />
+          <MagicTable magias={magias} onSelect={onSelect} selectedSkills={selectedSkills} />
         </div>
       </div>
     </div>
